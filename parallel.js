80ac13dead1733dc0e9a4bb1e3b428a58422ad2d
QuangHaTrieu <145729774+QuangHaTrieu@users.noreply.github.com>
2024-10-07 10:44:04 +1100

    Add files via upload

---
+++ parallel.js
@@ -0,0 +1,4 @@
+var vg_2 = "parallel.vg.json";
+vegaEmbed("#plot", vg_2).then(function(result) {
+}).catch(console.error);
+
---
+++ vega_lite_vis.js
@@ -0,0 +1,22 @@
+var vg_1 = "homework_w9.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+}).catch(console.error);
+
+var line_chart = "line chart.vg.json";
+vegaEmbed("#line", line_chart).then(function(result) {
+}).catch(console.error);
+
+var pie_chart = "pie_chart.vg.json";
+vegaEmbed("#pie", pie_chart).then(function(result) {
+}).catch(console.error);
+
+var slope_chart = "slope_chart.vg.json";
+vegaEmbed("#slope", slope_chart).then(function(result) {
+}).catch(console.error);
+
+var parallel_chart = "parallel.vg.json";
+vegaEmbed("#parallel", parallel_chart).then(function(result) {
+}).catch(console.error);
+
+
+
